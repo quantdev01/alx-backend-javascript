06d699fa4cc80ea600e6444588b39774e30791ab
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-06 01:35:16 +0200

    task 3

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,6 +2,8 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = add(newNeighborhood)=> self.sanFranciscoNeighborhoods.push(newNeighborhood);
-// return self.sanFranciscoNeighborhoods;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
