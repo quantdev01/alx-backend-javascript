b37a175f3bf695ab245a1af07e66aaea9be3b677
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-07 15:22:18 +0200

    question 2 solved, written correctly the script

---
+++ 0x00-ES6_basic/file/3-main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _defaultParameter = _interopRequireDefault(require("./3-default-parameter.js"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+console.log((0, _defaultParameter["default"])(34));
+console.log((0, _defaultParameter["default"])(34, 3));
+console.log((0, _defaultParameter["default"])(34, 3, 4));
