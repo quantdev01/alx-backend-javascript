b885031a3861857a04497cd78f75be2f030d0610
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-08 13:28:45 +0200

    Corrected task 8 to use property computed

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,14 @@
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
-
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
-return budget;
-
+  return budget;
 }
