fd711095073f978c95768b7026fe12a1cd1720e5
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-10 03:53:48 +0200

    task 4 done

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,26 @@
-export default class Currency
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(x) {
+    this._code = x;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(x) {
+    this._name = x;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
