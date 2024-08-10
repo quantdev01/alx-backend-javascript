d2ad243f96d7cc5cb7ecd94de0d369ab642b5fdc
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-10 03:24:07 +0200

    Task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(x) {
+    if (typeof x === 'string') {
+      this._name = x;
+    } else {
+      console.log('TypeError: Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(x) {
+    if (typeof x === 'number') {
+      this._length = x;
+    } else {
+      console.log('Must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(x) {
+    if (typeof x === 'object') {
+      this._students = x;
+    } else {
+      console.log('Must be an array object');
+    }
+  }
+}
