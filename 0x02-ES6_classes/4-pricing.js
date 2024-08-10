f22dfc608c4c213a929eec6b1e9a7a75f69c156d
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-08-10 04:08:22 +0200

    Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(x) {
+    this._amount = x;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(x) {
+    this._currency = x;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
