13a124182994f417728a22fda0bd7c7d04b06605
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-10-04 03:53:15 +0200

    Task 0

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,6 @@
+
 const readline = require('readline');
+process.stdin.resume();
 
 const r1 = readline.createInterface({
   input: process.stdin,
@@ -7,7 +9,10 @@ const r1 = readline.createInterface({
 
 r1.question("Welcome to Holberton School, what is your name?\n", function(name) {
   console.log("Your name is: " + name);
-  console.log("This important software is now closing");
 
   r1.close("Hey");
-})
+});
+
+process.stdin.on('end', () => {
+  console.log("This important software is now closing");
+});
---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+// 0-calcul.js
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
